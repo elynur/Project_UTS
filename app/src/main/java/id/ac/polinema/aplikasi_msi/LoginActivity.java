a7fecf4d902884063d8fddbd3cf25e8a6388323e
elynur <elynurrahayu@gmail.com>
2020-03-24 15:00:41 +0700

    Slider (Mencoba menggunakan fragment)

--- app/src/main/java/id/ac/polinema/aplikasi_msi/LoginActivity.java
+++ app/src/main/java/id/ac/polinema/aplikasi_msi/LoginActivity.java
@@ -2,15 +2,13 @@ package id.ac.polinema.aplikasi_msi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
