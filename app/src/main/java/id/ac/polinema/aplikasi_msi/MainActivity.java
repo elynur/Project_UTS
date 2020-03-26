c34f9b5e68300c8dab05e7f28c42e4cba357b38e
elynur <elynurrahayu@gmail.com>
2020-03-26 22:40:52 +0700

    Create Blank Fragment Home, Menu, Cart

--- app/src/main/java/id/ac/polinema/aplikasi_msi/MainActivity.java
+++ app/src/main/java/id/ac/polinema/aplikasi_msi/MainActivity.java
@@ -1,16 +1,24 @@
 package id.ac.polinema.aplikasi_msi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import android.view.View;
 
+import id.ac.polinema.aplikasi_msi.Fragment.CartFragment;
+import id.ac.polinema.aplikasi_msi.Fragment.HomeFragment;
+import id.ac.polinema.aplikasi_msi.Fragment.MenuFragment;
 import id.ac.polinema.aplikasi_msi.Model.Session;
 
 public class MainActivity extends AppCompatActivity {
 
     private Session session;
+
+    //Deklarasi fragment
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +33,32 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
             finish();
         }
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+
+        fragmentTransaction.replace(R.id.changeFrame,new HomeFragment());
+        fragmentTransaction.commit();
     }
+
+    public void handleHome(View view) {
+        Fragment fragmentManager = getSupportFragmentManager().findFragmentById(R.id.changeFrame);
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+
+        fragmentTransaction.replace(R.id.changeFrame,new HomeFragment());
+        fragmentTransaction.commit();
+    }
+
+    public void handleMenu(View view) {
+        Fragment fragmentManager = getSupportFragmentManager().findFragmentById(R.id.changeFrame);
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.changeFrame,new MenuFragment());
+        fragmentTransaction.commit();
+    }
+
+    public void handleCart(View view) {
+        Fragment fragmentManager = getSupportFragmentManager().findFragmentById(R.id.changeFrame);
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.changeFrame,new CartFragment());
+        fragmentTransaction.commit();
+    }
+
 }
